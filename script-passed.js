308d6feed9c8cbecad1bdac0384033def93c0540
azaan1134 <azjargala076@gmail.com>
2026-09-14 15:30:47 +0800

    Багшийн хэлсэн зүйлсийг засав.

---
+++ script-passed.js
@@ -0,0 +1,18 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+  vus: 30,
+  duration: "1m",
+  // baseline p95 = 2.43s * 1.5 = 3.650s босго утгаар авна
+  thresholds: {
+    http_req_duration: ['p(95)<3650'],
+    http_req_failed:   ['rate<0.01'],
+  },
+};
+
+export default function () {
+  const res = http.get('https://test.k6.io');
+  check(res, { 'status 200 байна': (r) => r.status === 200 });
+  sleep(1);
+}
